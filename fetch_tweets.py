4adbfc377fdd1dcdd89ea1e0f96b98e32d87c341
Saša Prole <proles@comtrade.com>
2018-03-21 12:38:17 +0100

    Moved fetching and saving logic to a separate module

--- fetch_tweets.py
+++ fetch_tweets.py
@@ -1,18 +1,12 @@
 import config
 import twitter
-import logging
+from tweet_fetcher import TweetFetcher
 from pymongo import MongoClient
-
-
-FORMAT = '%(asctime)-15s - %(levelname)s - %(message)s'
-logging.basicConfig(filename='test.log', format=FORMAT, level=logging.INFO)
 
 api = twitter.Api(consumer_key=config.consumer_key,
                   consumer_secret=config.consumer_secret,
                   access_token_key=config.access_token_key,
                   access_token_secret=config.access_token_secret)
-
-client = MongoClient()
 
 terms = ["Fashion Tech",
          "Wearables",
@@ -23,16 +17,11 @@ terms = ["Fashion Tech",
          "Visibility"]
 
 
+client = MongoClient()
 ct = client.cycle_tweets
 ct_search_results = ct.search_results
 
-for term in terms:
-    search_result = api.GetSearch(term=term, count=180)
-    logging.info("Completed search for '%s'. Number of tweets returned: %s", term, str(len(search_result)))
-    for status in search_result:
-        status = status.AsDict()
-        status['term'] = term
-        ct_search_results.find_one_and_update(
-                {'tweet_id':status['id']},
-                {'$set':status},
-                upsert=True)
+tf = TweetFetcher(terms, api, ct_search_results)
+
+tf.fetch()
+tf.save()
---
+++ tweet_fetcher.py
@@ -0,0 +1,28 @@
+import logging
+
+class TweetFetcher():
+    
+    FORMAT = '%(asctime)-15s - %(levelname)s - %(message)s'
+    logging.basicConfig(filename='test.log', format=FORMAT, level=logging.INFO)
+    def __init__(self, list_of_topics, twitter_api, collection):
+        self.twitter_api = twitter_api
+        self.list_of_topics = list_of_topics
+        self.collection = collection
+        
+    def fetch(self):
+        for topic in self.list_of_topics:
+            search_result = self.twitter_api.GetSearch(term=topic, count=180)
+            logging.info("Completed search for '%s'. Number of tweets returned: %s", topic, str(len(search_result)))
+            self.statuses = []
+            for status in search_result:
+                status = status.AsDict()
+                status['term'] = topic
+                self.statuses.append(status)
+            return self.statuses
+    
+    def save(self):
+        for status in self.statuses:
+            self.collection.find_one_and_update(
+                {'tweet_id':status['id']},
+                {'$set':status},
+                upsert=True)
