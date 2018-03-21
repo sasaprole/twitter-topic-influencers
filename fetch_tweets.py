44834a74a6c3d65f305226893dacf0caeda4426b
Saša Prole <proles@comtrade.com>
2018-03-21 12:28:43 +0100

    Fixed usage of wrong token

--- fetch_tweets.py
+++ fetch_tweets.py
@@ -1,11 +1,16 @@
 import config
 import twitter
+import logging
 from pymongo import MongoClient
+
+
+FORMAT = '%(asctime)-15s - %(levelname)s - %(message)s'
+logging.basicConfig(filename='test.log', format=FORMAT, level=logging.INFO)
 
 api = twitter.Api(consumer_key=config.consumer_key,
                   consumer_secret=config.consumer_secret,
                   access_token_key=config.access_token_key,
-                  access_token_secret=config.access_token_key)
+                  access_token_secret=config.access_token_secret)
 
 client = MongoClient()
 
@@ -23,7 +28,7 @@ ct_search_results = ct.search_results
 
 for term in terms:
     search_result = api.GetSearch(term=term, count=180)
-
+    logging.info("Completed search for '%s'. Number of tweets returned: %s", term, str(len(search_result)))
     for status in search_result:
         status = status.AsDict()
         status['term'] = term
